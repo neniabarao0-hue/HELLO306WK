0cfb71283abda177602e6100aee8f39dd4050705
anen <neniabarao0@gmail.com>
2025-11-28 06:49:17 +0000

    init

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, WORLD!");
+        System.out.println("I'm Aalliyah Audrey");
+        System.out.println("FIT CHECK!");
+        System.out.println("I love Computer Science so much!");
+    }
+}
